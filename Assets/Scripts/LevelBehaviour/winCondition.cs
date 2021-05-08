510060b9c745472ad37ae27c172493be8da8dcbc
JP <jprshaid123@gmail.com>
2021-05-08 20:58:09 -0300

    UI stuff + Win/Loss condition

---
+++ Assets/Scripts/LevelBehaviour/winCondition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player.Controller;
+using Player.Movement;
+
+public class winCondition : MonoBehaviour
+{
+    public WinLossManager winLossManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().stopMovement();
+            winLossManager.disableUI();
+            winLossManager.win();
+        }
+    }
+}
