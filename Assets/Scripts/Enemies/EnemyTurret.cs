ed968f1ff928028f96f8d37144c3cd8b33701083
JuanCristofalo <jccristofalo@gmail.com>
2021-05-09 17:31:14 -0300

    Enemy Turret Sound fixed

--- Assets/Scripts/Enemies/EnemyTurret.cs
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -12,12 +12,7 @@ public class EnemyTurret : MonoBehaviour
 
     public AudioSource mAudioSrc;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
