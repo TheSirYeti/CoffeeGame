56ae8a46f480c8a2bbb42cfd7e2c31a40b8d88ee
JuanCristofalo <jccristofalo@gmail.com>
2021-05-08 21:03:19 -0300

    First Turret Try
    - Made Enemy Turret
    - Animated it
    - Made him throw things (needs fix)

---
+++ Assets/Scripts/Enemies/EnemyBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+
+    public  float speed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position -= new Vector3 (transform.position.x, 0, transform.position.z) * speed * Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public  float fireRate      = 1f;
+    private float fireCountdown = 1.9f;
+
+    public GameObject bulletPrefab;
+    public Transform  firePoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 4.2f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime; 
+                
+    }
+
+    void Shoot()
+    {
+        Instantiate (bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
