b154dc4fbbeeefa8d743a1810b05d66ba4e39124
JP <jprshaid123@gmail.com>
2021-05-08 21:06:32 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/TheSirYeti/CoffeeGame into main""
    This reverts commit 67f363297d15a964198822b9cdddef4e419f2db5.

--- Assets/Scripts/UIelements/FadeManager.cs
+++ Assets/Scripts/UIelements/FadeManager.cs
@@ -6,17 +6,9 @@ using UnityEngine.SceneManagement;
 public class FadeManager : MonoBehaviour
 {
     public Animator animator;
-    public int sceneToLoad;
 
     public void fade()
     {
-        StartCoroutine(fading(sceneToLoad));
-    }
-
-    IEnumerator fading(int scene)
-    {
         animator.SetTrigger("fadeOut");
-        yield return new WaitForSeconds(0.75f);
-        SceneManager.LoadScene(scene);
     }
 }
