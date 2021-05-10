6a7a35291f047b7a2cab48269b71a29375457ccd
JP <jprshaid123@gmail.com>
2021-05-09 23:43:29 -0300

    Volumen

---
+++ Assets/Scripts/LevelBehaviour/saveVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saveVolume : MonoBehaviour
+{
+    public float volume;
+    public Slider slider;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        volume = slider.value;
+    }
+}
---
+++ Assets/Scripts/LevelBehaviour/volumeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumeManager : MonoBehaviour
+{
+    AudioSource[] sources;
+    float volume;
+
+    void Start()
+    {
+        sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        GameObject g = GameObject.FindWithTag("volume");
+        volume = g.GetComponent<saveVolume>().volume;
+    }
+
+    private void Update()
+    {
+        GameObject g = GameObject.FindWithTag("volume");
+        volume = g.GetComponent<saveVolume>().volume;
+        foreach (AudioSource a in sources)
+        {
+            a.volume = volume;
+        }
+    }
+}
