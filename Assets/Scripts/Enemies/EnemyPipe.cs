5f6653d69e0c13e2fa6f2ec82894f87da9064deb
JuanCristofalo <jccristofalo@gmail.com>
2021-06-19 20:17:04 -0300

    FirePipe working

---
+++ Assets/Scripts/Enemies/EnemyPipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPipe : MonoBehaviour
+{
+    public GameObject Fire;
+    public bool isActive;
+
+
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive == true)
+        {
+            Fire.SetActive(true);
+
+            Invoke("SetFalse", 6);
+            
+        }
+        else
+        {
+            Fire.SetActive(false);
+
+            Invoke("SetTrue", 6);
+        }
+    }
+
+    public void SetTrue()
+    {
+        isActive = true;
+    }
+
+    public void SetFalse()
+    {
+        isActive = false;
+    }
+}
