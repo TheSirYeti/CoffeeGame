0b32a35b1f0dfa9efbdb95f7d15ddf32ef8d8f3d
JuanCristofalo <jccristofalo@gmail.com>
2021-06-20 21:34:32 -0300

    Ball Animation corrected

--- Assets/Scripts/Enemies/EnemyTurret.cs
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class EnemyTurret : MonoBehaviour
 {
-    public  float fireRate      = 1f;
-    private float fireCountdown = 1.9f;
+    
 
     public GameObject bulletPrefab;
     public Transform  firePoint;
     void Update()
     {
-        if(fireCountdown <= 0f)     //If it's allowed to fire, it fires
-        {
-            Shoot();
-            fireCountdown = 4.2f / fireRate;
-
-            SoundManager.instance.PlaySound(SoundID.PAPER_BALL);       //Plays a sound effect
-        }
-
-        fireCountdown -= Time.deltaTime;
+    
     }
 
     void Shoot()
     {
-        Instantiate (bulletPrefab, firePoint.position, firePoint.rotation);     //Instantiates the bullet on the desired position
+        Instantiate (bulletPrefab, firePoint.position, firePoint.rotation); //Instantiates the bullet on the desired position
+        SoundManager.instance.PlaySound(SoundID.PAPER_BALL);  
     }
+          
 }
