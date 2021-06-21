0b0f4ff83926d87aac9d2446ce8bee7793603712
JP <jprshaid123@gmail.com>
2021-06-20 22:14:41 -0300

    Fixed losing and winning conditions

---
+++ Assets/Scripts/music/PlayMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    void Start()
+    {
+        if (!SoundManager.instance.isMusicPlaying(MusicID.MAINSONG))
+        {
+            SoundManager.instance.PlayMusic(MusicID.MAINSONG);
+        }
+    }
+}
