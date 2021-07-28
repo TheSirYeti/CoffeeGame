7ed17b9068319629c7a58cc210c56065715c856d
JuanCristofalo <jccristofalo@gmail.com>
2021-07-28 18:02:02 -0300

    Crane Simple Movement + BossLevel idea

---
+++ Assets/Scripts/Enemies/EnemyCrane.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCrane : MonoBehaviour
+{
+    public Vector3 craneRotationValues;
+
+    public Transform crane;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        crane.Rotate(craneRotationValues);
+    }
+}
